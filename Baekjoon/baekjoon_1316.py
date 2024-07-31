8a1fd6eb2ba292f64f22f44c4eb7337331cae62f
chosoobin37 <angelajsb@naver.com>
2024-07-31 23:52:28 +0900

    그룹 단어

--- Baekjoon/baekjoon_1316.py
+++ Baekjoon/baekjoon_1316.py
@@ -2,14 +2,15 @@ num = int(input())
 count = 0
 
 for i in range(num):
-    word = input().strip()
+    word = input()
     is_group = True
+
     for j in range(len(word)-1):
         if word[j] != word[j+1]:
             if word[j+1] in word[:j+1]:
                 is_group = False
                 break
-    if is_group:
-        count +=1
+    if is_group == True:
+        count += 1 
 
 print(count)
