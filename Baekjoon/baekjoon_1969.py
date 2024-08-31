e77234a7c20f3f8e0edd934b9041dd76b6785382
chosoobin37 <angelajsb@naver.com>
2024-08-31 23:45:32 +0900

    DNA 거리

---
+++ Baekjoon/baekjoon_1969.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin
+
+dna, length = map(int, input().split())
+dnas = []
+
+for i in range(dna):
+    atgc = input().strip()
+    dnas.append(atgc)
+
+result_dna = []
+distance_sum = 0
+
+for i in range(length):
+    count = {'A': 0, 'T':0, 'G':0, 'C':0}
+
+    for j in range(dna):
+        count[dnas[j][i]] += 1
+
+    max_char = max(count, key=lambda x: (count[x], -ord(x)))
+    result_dna.append(max_char)
+
+    distance_sum += dna - count[max_char]
+
+print("".join(result_dna))
+print(distance_sum)
