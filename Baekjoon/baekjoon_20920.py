818e5d947c4a1a8195cbebfecba367d424e595fb
chosoobin37 <angelajsb@naver.com>
2024-09-06 00:11:46 +0900

    영단어장

--- Baekjoon/baekjoon_20920.py
+++ Baekjoon/baekjoon_20920.py
@@ -13,6 +13,7 @@ for i in range(words):
 word_count = Counter(eng)
 
 # 중복제거 하는 과정을 시간복잡도 줄이기 위해 list -> set -> list
+# set -> 별도 알고리즘 필요 없이 자동으로 중복제거
 result = list(set(eng))
 
 # 자주 등장한 순, 길이가 긴 순, 사전 순 -> 우선순위 적용
